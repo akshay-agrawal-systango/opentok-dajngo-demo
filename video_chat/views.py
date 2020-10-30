d55bf1f0c8e4f71ad05a595e5d4be789409f4ffe
Akshay Agrawal <aagrawal@isystango.com>
2020-10-30 18:02:22 +0530

    new token each time

--- video_chat/views.py
+++ video_chat/views.py
@@ -11,14 +11,13 @@ def index(request):
 def video_chat_room(request, room_id):
     session_obj = OpenTokSessions.objects.get(id=room_id)
     session_id = session_obj.session_id
-    token = session_obj.opentoktokens_set.all().last().token
+    opentok = OpenTok(settings.API_KEY, settings.PEOJECT_SECRET)
+    token = opentok.generate_token(session_id)
     context = {'token':token, 'session_id': session_id, 'api_key': settings.API_KEY}
     return render(request, 'video_chat_room.html', context)
 
 def create_video_chat_room(request):
     opentok = OpenTok(settings.API_KEY, settings.PEOJECT_SECRET)
     session = opentok.create_session(media_mode=MediaModes.routed)
-    token = opentok.generate_token(session.session_id)
     session_obj = OpenTokSessions.objects.create(session_id=session.session_id)
-    session_obj.opentoktokens_set.create(token=token)
     return redirect('video_chat_room', room_id=session_obj.id)
